c7c165f5597313311393ea2ba8828ba786f7ed29
Ubuntu <azureuser@master.sixjf0zfhwwuliz2e4nmzbijgf.bx.internal.cloudapp.net>
2024-07-20 17:13:59 +0000

    seventh commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def my_event_handler(data):
     # Do something here
     pass
 
-r = ansible_runner.run(private_data_dir='./private', playbook='p2.yml', artifacts_handler=my_artifacts_handler, status_handler=my_status_handler, event_handler=my_event_handler)
+r = ansible_runner.run(private_data_dir='./private', playbook='p1.yml', artifacts_handler=my_artifacts_handler, status_handler=my_status_handler, event_handler=my_event_handler)
 print("{}: {}".format(r.status, r.rc))
 
 for each_host_event in r.events:
